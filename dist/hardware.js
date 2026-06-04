b1f6baf2f4e995620dbab326bba0bff099a6d580
snyeg <99054951+snqwq@users.noreply.github.com>
2026-06-04 18:12:16 -0500

    add platform parsing and comments

--- dist/hardware.js
+++ dist/hardware.js
@@ -1,22 +1,27 @@
+// setup invisible canvas with webgl
 const canvas = document.getElementById("info-canvas");
 const gl = canvas.getContext("webgl");
 
+// get elements
 const vendorElement = document.getElementById("GPU-vendor");
 const rendererElement = document.getElementById("GPU-renderer");
+const OSElement = document.getElementById("OS-type");
 
+const platform = parsePlatform();
 const vendor = parseVendor(getUnmaskedInfo(gl).vendor);
 const renderer = parseRenderer(getUnmaskedInfo(gl).renderer);
+
+// update ui with values
+vendorElement.textContent = "Vendor: " + vendor;
+rendererElement.textContent = "GPU: " + renderer;
+OSElement.textContent = "OS: " + platform;
+
 // console.log(getUnmaskedInfo(gl));
 console.log(parseVendor(getUnmaskedInfo(gl).vendor));
 // console.log(navigator.gpu);
 console.log(getUnmaskedInfo(gl).renderer);
 console.log(navigator.deviceMemory);
-console.log(navigator.platform);
 console.log(navigator.hardwareConcurrency);
-
-vendorElement.textContent = "Vendor: " + vendor;
-rendererElement.textContent = "GPU: " + renderer;
-
 
 function getUnmaskedInfo(gl) {
   let unMaskedInfo = {
@@ -39,11 +44,29 @@ function parseVendor(unMaskedVendor) {
   let vendor = "";
 
   if (unMaskedVendor.toLowerCase().includes("nvidia")) {
-    vendor = "nvidia";
-  } else vendor = "unknown";
+    vendor = "NVIDIA";
+  } else vendor = unMaskedVendor + " (vendor parsing failed)";
   return vendor;
 }
 
 function parseRenderer(unMaskedVendor) {
-  return unMaskedVendor
+  return unMaskedVendor;
+}
+
+function parsePlatform() {
+  let platform = "";
+
+  if (navigator.platform == "Win32") {
+    platform = "Windows";
+  } else if (navigator.platform == "MacIntel") {
+    platform = "Mac OS (Intel)";
+  } else if (navigator.platform == "Linux x86_64") {
+    platform = "Linux (x86_64)";
+  } else if (navigator.platform == "Linux armv81") {
+    platform = "Linux (ARM)";
+  } else {
+    platform = navigator.platform;
+  }
+
+  return platform;
 }
